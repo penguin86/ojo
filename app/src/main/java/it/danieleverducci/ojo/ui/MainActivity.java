63b08fe718626af3b9211fe4a75a459f517eee4e
Daniele Verducci (Slimpenguin) <daniele.verducci@gmail.com>
2022-01-31 08:35:57 +0100

    Navigation refactoring

--- app/src/main/java/it/danieleverducci/ojo/ui/MainActivity.java
+++ app/src/main/java/it/danieleverducci/ojo/ui/MainActivity.java
@@ -2,22 +2,15 @@ package it.danieleverducci.ojo.ui;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import it.danieleverducci.ojo.R;
 import it.danieleverducci.ojo.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         // Show FAB only on first fragment
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-            if (destination.getId() == R.id.FirstFragment)
+            if (destination.getId() == R.id.HomeFragment)
                 binding.fab.show();
             else
                 binding.fab.hide();
@@ -42,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                navController.navigate(R.id.action_FirstFragment_to_SecondFragment);
+                navController.navigate(R.id.action_homeToSettings);
             }
         });
     }
